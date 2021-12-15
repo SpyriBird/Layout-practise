db3104e5a39edd65c177576cea6f03e71d7d12e5
Tanya <t.goreva@g.nsu.ru>
2021-12-15 19:30:15 +0700

    installed pug and pug-loader

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+const path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: 'output.html',
+            minify: false,
+            template: 'index.pug'
+        }),
+        new HtmlWebpackPugPlugin(),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(woff|ttf)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            }
+        ]
+    }
+}
