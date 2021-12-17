a9717754d200221c4fa782ecf5c83628fc8f2b6b
Tanya <t.goreva@g.nsu.ru>
2021-12-17 16:44:42 +0700

    trying sass and devserver

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-import './style.css'
+import './style.sass'
 import * as $ from 'jquery'
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,74 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-        context: path.resolve(__dirname, 'src'),
-        mode: 'development',
-        entry: {
-            main: './index.js'
-        },
-        output: {
-            filename: '[name].[contenthash].js',
-            path: path.resolve(__dirname, 'dist')
-        },
-        plugins: [
-            new HTMLWebpackPlugin({
-                filename: 'output.html',
-                minify: false,
-                template: 'index.pug'
-            }),
-            new HtmlWebpackPugPlugin(),
-            new CleanWebpackPlugin()
-        ],
-        module: {
-            rules: [{
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader']
-                },
-                {
-                    test: /\.(png|jpg|svg|gif)$/,
-                    use: ['file-loader']
-                },
-                {
-                    test: /\.(woff|ttf)$/,
-                    use: ['file-loader']
-                },
-                {
-                    test: /\.pug$/,
-                    loader: 'pug-loader'
-                },
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        // Creates `style` nodes from JS strings
-                        "style-loader",
-                        // Translates CSS into CommonJS
-                        "css-loader",
-                        // Compiles Sass to CSS
-                        "sass-loader",
-                    ],
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    devServer: {
+        port: 'auto',
+        open: true,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: 'output.html',
+            minify: false,
+            template: 'index.pug'
+        }),
+        new HtmlWebpackPugPlugin(),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    // options: {
+                    //     hmr: true,
+                    //     reloadAll: true
+                    // }
+                }, 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(woff|ttf)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader'
                 ]
             }
-        }
+        ]
+    }
+}
