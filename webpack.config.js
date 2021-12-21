f61d9140a3bed1e781bf7802a2222f076cc65b5c
Tanya <t.goreva@g.nsu.ru>
2021-12-21 12:47:05 +0700

    added semantics, added html-rule

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|gif)$/,
-                use: ['file-loader']
+                use: ['url-loader']
             },
             {
                 test: /\.(woff|ttf|svg)$/,
