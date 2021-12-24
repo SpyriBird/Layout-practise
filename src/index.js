78869b446786038dede9696194bc0b0ce2158d75
Tanya <t.goreva@g.nsu.ru>
2021-12-24 23:20:55 +0700

    added sign-in-window

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './style.sass'
 import * as $ from 'jquery'
+
+sign_in.onclick = function() {
+    $(".search-window-container").attr('style', 'display: none');
+    $(".sign-in-window-container").attr('style', 'display: block')
+
+};
