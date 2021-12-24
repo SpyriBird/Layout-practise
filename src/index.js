209a14ab19af1631fab30c72ca0ddca2629a3cf2
Tanya <t.goreva@g.nsu.ru>
2021-12-24 23:59:20 +0700

    added register popup, renamed popups, added input type=radio

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import './style.sass'
 import * as $ from 'jquery'
 
 sign_in.onclick = function() {
-    $(".search-window-container").attr('style', 'display: none');
-    $(".sign-in-window-container").attr('style', 'display: block')
+    $(".main__search-popup").attr('style', 'display: none');
+    $(".main__sign-in-popup").attr('style', 'display: block')
+
+};
+
+register.onclick = function() {
+    $(".main__search-popup").attr('style', 'display: none');
+    $(".main__register-popup").attr('style', 'display: block')
 
 };
