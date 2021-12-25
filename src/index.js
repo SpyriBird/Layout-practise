f38d8c535959a8245732f29c9dd2d665e3076e9b
Tanya <t.goreva@g.nsu.ru>
2021-12-25 22:23:26 +0700

    added toggle element (needs further work)

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import './style.sass'
 import * as $ from 'jquery'
 
 sign_in.onclick = function() {
-    $(".main__search-popup").attr('style', 'display: none');
-    $(".main__sign-in-popup").attr('style', 'display: block')
+    document.getElementById('3').style.display = 'none';
+    document.getElementById('1').style.display = 'block';
 
 };
 
 register.onclick = function() {
-    $(".main__search-popup").attr('style', 'display: none');
-    $(".main__register-popup").attr('style', 'display: block')
-
+    document.getElementById('3').style.display = 'none';
+    document.getElementById('2').style.display = 'block';
 };
