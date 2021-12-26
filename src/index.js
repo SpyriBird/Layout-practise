01fd1444ee2d4e9d63ea022922dbf0bba0653abb
Tanya <t.goreva@g.nsu.ru>
2021-12-26 13:27:17 +0700

    added click handler to register popup sign in button

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import './style.sass'
 import * as $ from 'jquery'
 
-sign_in.onclick = function() {
+function showSignInPopup() {
     $(".main__search-popup").attr('style', 'display: none');
     $(".main__register-popup").attr('style', 'display: none');
-    $(".main__sign-in-popup").attr('style', 'display: block')
+    $(".main__sign-in-popup").attr('style', 'display: block');
     return false;
 };
 
-register.onclick = function() {
+function showRegisterPopup() {
     $(".main__search-popup").attr('style', 'display: none');
     $(".main__register-popup").attr('style', 'display: block');
-    $(".main__sign-in-popup").attr('style', 'display: none')
+    $(".main__sign-in-popup").attr('style', 'display: none');
     return false;
 };
+
+$(".sign-in-button").on("click", showSignInPopup);
+$(".register-button").on("click", showRegisterPopup);
